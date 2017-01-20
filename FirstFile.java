4cd93e2517b31faff3f8669cbe2bc061a2396948
manjunathp091 <manjunath@knstek.com>
2017-01-20 15:16:40 +0530

    added FirstFile.java

---
+++ FirstFile.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class FirstFile {
+
+	public static void main(String[] args) {
+		System.out.println("// TODO Auto-generated method stub");
+		System.out.println("out look");
+
+	}
+
+}
